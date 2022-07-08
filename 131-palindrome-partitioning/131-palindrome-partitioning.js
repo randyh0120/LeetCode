e2305f452eb9210bdb1e013c9aa70d1a13dda0ec
Randy Hattab <randyhattab.one@gmail.com>
2022-07-07 22:00:39 -0400

    Time: 542 ms (12.49%), Space: 79.8 MB (79.97%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} s
+ * @return {string[][]}
+ */
+var partition = function(s) {
+    let res = [];
+    
+    function isPalindrome(subStr) {
+        if (subStr.length === 1) return true;
+        
+        let left = 0;
+        let right = subStr.length - 1;
+        
+        while (left < right) {
+            if (subStr[left] !== subStr[right]) return false;
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+    
+    function backtrack(str, arr) {
+        if (!str.length) res.push(arr);
+        
+        for (let i = 0; i < str.length; i++) {
+            const subStr = str.slice(0, i + 1); // Make sure we at least have once char
+            
+            if (isPalindrome(subStr)) {
+                backtrack(str.slice(i + 1), arr.concat(subStr));
+            }
+        }
+    }
+    
+    backtrack(s, []);
+    
+    return res;
+};
