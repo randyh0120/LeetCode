ebf176784924782b28b4295039f43dc6e4434ddc
Randy Hattab <randyhattab.one@gmail.com>
2022-07-07 21:44:31 -0400

    Time: 459 ms (84.08%), Space: 42.2 MB (91.99%) - LeetHub

---
+++ 79-word-search/79-word-search.js
@@ -0,0 +1,42 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+    let ROWLEN = board.length;
+    let COLLEN = board[0].length;
+    
+    function backtrack(row, col, wordLength) {
+        if (wordLength === word.length) return true;
+        
+        if (row < 0 
+            || row >= ROWLEN 
+            || col < 0 
+            || col >= COLLEN 
+            || board[row][col] !== word[wordLength]) {
+            return false;
+        }
+        
+        let temp = board[row][col];
+        
+        board[row][col] = '#';
+        
+        if (backtrack(row - 1, col, wordLength + 1)
+           || backtrack(row + 1, col, wordLength + 1)
+           || backtrack(row, col - 1, wordLength + 1)
+           || backtrack(row, col + 1, wordLength + 1)) {
+            return true;
+        }
+        
+        board[row][col] = temp;
+    }
+    
+    for (let i = 0; i < ROWLEN; i++) {
+        for (let j = 0; j < COLLEN; j++) {
+            if (backtrack(i, j, 0)) return true;
+        }
+    }
+    
+    return false;
+};
