4798d8ab69756d0f5d6fc800dade6c0d5c5a2fed
Randy Hattab <randyhattab.one@gmail.com>
2022-07-09 16:28:46 -0400

    Time: 103 ms (67.37%), Space: 45.3 MB (48.58%) - LeetHub

--- 51-n-queens/51-n-queens.js
+++ 51-n-queens/51-n-queens.js
@@ -7,22 +7,21 @@
 var solveNQueens = function(n) {
     let res = [];
     
-    function backtrack(board, r) {
-        if (r === n) {
+    function backtrack(board, row) {
+        if (row === n) {
             // - 1 to account for adding a Q that takes up a space
-            res.push(board.map((c) => '.'.repeat(c) + 'Q' + '.'.repeat(n - c - 1)));
-            return;
+            res.push(board.map((col) => '.'.repeat(col) + 'Q' + '.'.repeat(n - col - 1)));
         }
         
-        for (let c = 0; c < n; c++) {
+        for (let col = 0; col < n; col++) {
             // bc is the current element
             // br is the index of the element bc
             //
             // bc === c | checks row and col
             // bc === c - r + br | checks lower diagonal
             // bc === c + r - br | checks upper diagonal
-            if (!board.some((bc, br) => bc === c || bc === c - r + br || bc === c + r - br)) {
-                backtrack(board.concat(c), r + 1);
+            if (!board.some((bc, br) => bc === col || bc === col - row + br || bc === col + row - br)) {
+                backtrack(board.concat(col), row + 1);
             }
         }
     }
@@ -30,4 +29,4 @@ var solveNQueens = function(n) {
     backtrack([], 0);
     
     return res;
-};
+}
