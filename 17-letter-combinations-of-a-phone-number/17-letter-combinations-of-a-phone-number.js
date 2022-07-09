ae047c23b955e6768511c86df1bffefa577e3e03
Randy Hattab <randyhattab.one@gmail.com>
2022-07-08 23:58:04 -0400

    Time: 106 ms (13.58%), Space: 42.3 MB (19.95%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    if (!digits.length) return [];
+    
+    const map = {
+        2: ['a', 'b', 'c'],
+        3: ['d', 'e', 'f'],
+        4: ['g', 'h', 'i'],
+        5: ['j', 'k', 'l'],
+        6: ['m', 'n', 'o'],
+        7: ['p', 'q', 'r', 's'],
+        8: ['t', 'u', 'v'],
+        9: ['w', 'x', 'y', 'z'],
+    }
+    
+    let res = [];
+    
+    function backtrack(str, idx) {
+        if (idx === digits.length) {
+            res.push(str);
+            return;
+        }
+        
+        for (const char of map[digits[idx]]) {
+            backtrack(str + char, idx + 1);
+        }
+    }
+    
+    backtrack("", 0);
+    
+    return res;
+};
