a8540a5d0e05ffa73e2076ab277b2bff49b5463c
Randy Hattab <randyhattab.one@gmail.com>
2022-07-10 15:16:19 -0400

    Time: 138 ms (26.39%), Space: 46.3 MB (20.20%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var orangesRotting = function(grid) {
+    if (!grid.length) return -1;
+    
+    let minutes = 0;
+    let lenR = grid.length;
+    let lenC = grid[0].length;
+    let queue = [];
+    let dir = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+    let fresh = 0;
+    
+    for (let i = 0; i < lenR; i++) {
+        for (let j = 0; j < lenC; j++) {
+            if (grid[i][j] === 2) queue.push([i, j]);
+            if (grid[i][j] === 1) fresh += 1;
+        }
+    }
+    
+    while (fresh && queue.length) {
+        let next = [];
+        
+        for (const [row, col] of queue) {
+            for (const [rd, cd] of dir) {
+                const r = row + rd;
+                const c = col + cd;
+                
+                if (r < 0 || r >= lenR || c < 0 || c >= lenC || grid[r][c] === 0 || grid[r][c] === 2) {
+                    continue;
+                }
+                
+                fresh -= 1;
+                grid[r][c] = 2;
+                next.push([r, c]);
+            }
+        }
+        
+        minutes += 1;
+        queue = next;
+    }
+    
+    return fresh > 0 ? -1 : minutes;
+};
