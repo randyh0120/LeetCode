af7f4afcf6cf9df54ad8cc5f77735d1aff3ae73d
Randy Hattab <randyhattab.one@gmail.com>
2022-07-11 20:47:59 -0400

    Time: 106 ms (98.04%), Space: 51 MB (26.51%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.js
+++ 286-walls-and-gates/286-walls-and-gates.js
@@ -5,34 +5,74 @@
 
 // time O(m * n) | sppace O(m * n)
 var wallsAndGates = function(rooms) {
+    // DFS
+//     let inf = 2147483647;
+//     let wall = -1;
+//     let gate = 0;
+//     let r = rooms.length;
+//     let c = rooms[0].length;
+    
+//     for (let i = 0; i < r; i++) {
+//         for (let j = 0; j < c; j++) {
+//             if (rooms[i][j] === gate) {
+//                 dfs(i, j, 0);
+//             }
+//         }
+//     }
+    
+//     return rooms;
+    
+//     function dfs(row, col, step) {
+//         if (row < 0 || row >= r || col < 0 || col >= c || rooms[row][col] === wall 
+//             || (step && rooms[row][col] <= step)) {
+//             return 0;
+//         }
+        
+//         rooms[row][col] = step;
+        
+//         dfs(row - 1, col, step + 1);
+//         dfs(row + 1, col, step + 1);
+//         dfs(row, col - 1, step + 1);
+//         dfs(row, col + 1, step + 1);
+//     }
+    
+    // BFS
     let inf = 2147483647;
     let wall = -1;
     let gate = 0;
     let r = rooms.length;
     let c = rooms[0].length;
+    let queue = [];
+    let step = 0;
+    const dir = [[-1, 0], [1, 0], [0, -1], [0, 1]];
     
     for (let i = 0; i < r; i++) {
         for (let j = 0; j < c; j++) {
             if (rooms[i][j] === gate) {
-                dfs(i, j, 0);
+                queue.push([i, j]);
             }
         }
     }
     
-    return rooms;
-    
-    function dfs(row, col, step) {
-        if (row < 0 || row >= r || col < 0 || col >= c || rooms[row][col] === wall 
-            || (step && rooms[row][col] <= step)) {
-            return 0;
+    while (queue.length) {
+        let next = [];
+        
+        for (const [row, col] of queue) {
+            for (const [dr, dc] of dir) {
+                const curR = row + dr;
+                const curC = col + dc;
+                
+                if (curR < 0 || curR >= r || curC < 0 || curC >= c || rooms[curR][curC] !== inf) {
+                    continue;
+                }
+                
+                rooms[curR][curC] = step + 1;
+                next.push([curR, curC]);
+            }
         }
         
-        rooms[row][col] = step;
-        
-        dfs(row - 1, col, step + 1);
-        dfs(row + 1, col, step + 1);
-        dfs(row, col - 1, step + 1);
-        dfs(row, col + 1, step + 1);
+        step += 1;
+        queue = next;
     }
 };
 
