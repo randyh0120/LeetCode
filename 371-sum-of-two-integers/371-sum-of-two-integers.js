2d792d88cd07ec306f328d0f8ec1feac20186629
Randy Hattab <randyhattab.one@gmail.com>
2022-07-12 21:43:26 -0400

    Time: 84 ms (49.88%), Space: 42.4 MB (5.64%) - LeetHub

---
+++ 371-sum-of-two-integers/371-sum-of-two-integers.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+    while (b) {
+        let temp = (a & b) << 1
+        a = a ^ b;
+        b = temp;
+    }
+    
+    return a;
+};
+
+// 1001 - 9
+//+1011 - 11
+// ----
+// 0010  xor
+// 1001  &
+//10010  & << 1
+//
+// 0010 - 1
+//10010 - 17
+// ----
+//10000 - 16
+//00100 - 4
+//-----
+//10100 = 20
+//
