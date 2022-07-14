e910fa5349e2bec0d539fc1b312ce9b3fbcb7dae
Randy Hattab <randyhattab.one@gmail.com>
2022-07-13 22:23:11 -0400

    Time: 97 ms (68.57%), Space: 43.9 MB (71.97%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// time O(n) | space O(1)
+var jump = function(nums) {
+    let res = 0;
+    let curr = -1;
+    let next = 0;
+    let target = nums.length - 1
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (next >= target) break;
+        
+        if (i > curr) res++, curr = next;
+        
+        next = Math.max(next, nums[i] + i);
+    }
+    
+    return res;
+};
